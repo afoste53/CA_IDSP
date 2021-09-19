f1a0ab1492fe407eb83a9b9881d51310fda30266
afoste53 <74116498+afoste53@users.noreply.github.com>
2021-09-19 18:08:20 -0500

    Fix comment issues in p4

--- proj4_queries.sql
+++ proj4_queries.sql
@@ -1,22 +1,22 @@
-/** Date pulled from
-https://drive.google.com/file/d/1rydAepWim_Pi0Krh1-RoJjJjDKwc4Gpv/view **/
+-- Date pulled from
+-- https://drive.google.com/file/d/1rydAepWim_Pi0Krh1-RoJjJjDKwc4Gpv/view 
 
-/**how mant distinct zip-codes are there?**/
+-- How mant distinct zip-codes are there? 
 SELECT COUNT(DISTINCT zip_code) AS 'Distinct Zip Codes' from home_value_data;
 
-/**How many zips from each state?**/
+-- How many zips from each state?
 SELECT state, COUNT(zip_code) from home_value_data GROUP BY 1;
 
-/**What range of years are represented in the dataset**/
+-- What range of years are represented in the dataset
 SELECT MIN(substr(date, 1,4)) AS 'Start Year', MAX(substr(date, 1,4)) AS 'End Year' from home_value_data;
 
-/**Select Min and Max values for each state in the last month available**/
+-- Select Min and Max values for each state in the last month available
 SELECT DISTINCT state, MIN(value), MAX(value) FROM home_value_data
 WHERE date = (SELECT MAX(date) FROM home_value_data)
 GROUP BY 1;
 
-/**Using the most recent month of data available, which states have the highest average home values? How about the lowest?, Bottom 10**/
-/** Top 10 **/
+-- Using the most recent month of data available, which states have the highest average home values? How about the lowest?, Bottom 10
+-- Top 10 
 SELECT state, ROUND(AVG(value),0) AS 'Average Value' 
 FROM home_value_data
 WHERE date = (SELECT MAX(date) from home_value_data)
@@ -24,7 +24,7 @@ GROUP BY 1
 ORDER BY 2 DESC
 LIMIT 10;
 
-/** Bottom 10 **/
+-- Bottom 10 
 SELECT state, ROUND(AVG(value),0) AS 'Average Value' 
 FROM home_value_data
 WHERE date = (SELECT MAX(date) from home_value_data)
@@ -32,7 +32,7 @@ GROUP BY 1
 ORDER BY 2
 LIMIT 10;
 
-/**Which states have the highest and lowest average home value in 2017?**/
+-- Which states have the highest and lowest average home value in 2017?
 WITH average_tab AS(
 SELECT state, ROUND(AVG(value), 0) AS averages
 FROM home_value_data
@@ -42,4 +42,3 @@ ORDER BY 2 DESC
 )
 SELECT * FROM average_tab
 WHERE averages=(SELECT MAX(averages) FROM average_tab) OR  averages=(SELECT MIN(averages) FROM average_tab);
-
